9db1d4a0398dba567a8c633ec5765ac494a6ea8d
dmdwns2 <ssreki1@naver.com>
2022-10-26 16:29:38 +0900

    MavenSpringExercise 221026
    port 8081로 변경 후 /hello 접속 시 Helloworld 출력

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -1,15 +1,29 @@
 package com.example.hello.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/get-api")
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(){
         return "Hello world";
     }
 
+    @GetMapping(value = "/name")
+    public String getName(){
+        return "Eungjun";
 
+    }
+
+    @GetMapping(value = "/variable1/{variable}")
+    public String getvariable1(@PathVariable String variable){
+        return variable;
+    }
+
+    @GetMapping(value = "/variable2/{variable}")
+    public String getvariable2(@PathVariable("variable") String var){
+        return var;
+    }
 }
