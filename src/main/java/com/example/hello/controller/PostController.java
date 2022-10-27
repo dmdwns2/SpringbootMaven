eb2618898ddf913ab95a28f8b3e5065753cae72a
dmdwns2 <ssreki1@naver.com>
2022-10-27 14:15:06 +0900

    MavenSpringExercise 221027
    MemberDto를 받는 postMember2 추가

--- src/main/java/com/example/hello/controller/PostController.java
+++ src/main/java/com/example/hello/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.example.hello.controller;
 
 
+import com.example.hello.domain.dto.MemberDto;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -19,8 +20,8 @@ public class PostController {
     public String postExample() {
         return "hello Post API";
     }
-
-    @PostMapping
+    // http://localhost:8080/api/v1/post-api/member1
+    @PostMapping(value = "/member1")
     public String postMember(@RequestBody Map<String, Object> postData) { // Object로 하는이유 어떤 타입으로 들어올 지 서버 입장에서는 몰라서
         StringBuilder sb = new StringBuilder();
 
@@ -29,5 +30,10 @@ public class PostController {
         });
         return sb.toString();
 
+    }
+    // http://localhost:8080/api/v1/post-api/member2
+    @PostMapping(value = "/member2")
+    public MemberDto postMember2(@RequestBody MemberDto memberDto) {
+        return memberDto;
     }
 }
