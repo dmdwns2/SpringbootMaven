1703f8e3388ba862be4df7a6bbb89f6bf9da8c69
dmdwns2 <ssreki1@naver.com>
2022-10-27 11:25:07 +0900

    MavenSpringExercise 221027
    getRequestParam1 추가

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -5,25 +5,34 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/get-api")
 public class HelloController {
-//
+    //
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello(){
+    public String hello() {
         return "Hello world";
     }
 
     @GetMapping(value = "/name")
-    public String getName(){
+    public String getName() {
         return "Eungjun";
 
     }
 
     @GetMapping(value = "/variable1/{variable}")
-    public String getvariable1(@PathVariable String variable){
+    public String getvariable1(@PathVariable String variable) {
         return variable;
     }
 
     @GetMapping(value = "/variable2/{variable}")
-    public String getvariable2(@PathVariable("variable") String var){
+    public String getvariable2(@PathVariable("variable") String var) {
         return var;
+    }
+
+    @GetMapping(value = "/request1")
+    public String getRequestParam1(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String organization
+    ) {
+        return name + " " + email + " " + organization;
     }
 }
