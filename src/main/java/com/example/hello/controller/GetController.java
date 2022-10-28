99b7672774e674bd90fd45ebbcc96c4f406ec02e
dmdwns2 <ssreki1@naver.com>
2022-10-28 11:33:06 +0900

    [RESTful API] MavenSpringExercise
    application.yml에 logging level 설정 코드 추가
    
    @Slf4j 어노테이션으로 호출시 로그 남김
    log.info

--- src/main/java/com/example/hello/controller/GetController.java
+++ src/main/java/com/example/hello/controller/GetController.java
@@ -1,32 +1,39 @@
 package com.example.hello.controller;
 
 import com.example.hello.domain.dto.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
+@Slf4j
 public class GetController {
     //
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
+
     public String hello() {
+        log.info("hello로 요청이 들어왔습니다.");
         return "Hello world";
     }
 
     @GetMapping(value = "/name")
+
     public String getName() {
+        log.info("getName으로 요청이 들어왔습니다.");
         return "Eungjun";
 
     }
 
     @GetMapping(value = "/variable1/{variable}")
     public String getvariable1(@PathVariable String variable) {
-        return variable;
+        log.info("getvariable1로 요청이 들어왔습니다."); return variable;
     }
 
     @GetMapping(value = "/variable2/{variable}")
     public String getvariable2(@PathVariable("variable") String var) {
+        log.info("getvariable2로 요청이 들어왔습니다.");
         return var;
     }
 
@@ -37,6 +44,7 @@ public class GetController {
                                     @RequestParam String email,
                                     @RequestParam String organization
     ) {
+        log.info("getRequestParam1으로 요청이 들어왔습니다.");
         return name + " " + email + " " + organization; // return하게 하기
     }
 
@@ -48,6 +56,7 @@ public class GetController {
             System.out.printf("key:%s value:%s\n",
                     map.getKey(), map.getValue());
         });
+        log.info("getRequestParam2으로 요청이 들어왔습니다.");
         return "request2가 호출 완료 되었습니다.";
 
     }
@@ -55,6 +64,8 @@ public class GetController {
     //http://localhost:8080/api/v1/get-api/request3?name=dmdwns&email=dmdwns@dmdwns.com&organization=멋사&age=29
     @GetMapping(value = "/request3")
     public String getRequestParam3(MemberDto memberDto) {
+
+        log.info("getRequestParam3으로 요청이 들어왔습니다.");
         return memberDto.toString();
     }
 }
