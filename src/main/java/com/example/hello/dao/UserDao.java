6835aba3b8dd4ccd4f63703bba7f822c90dbc07b
dmdwns2 <ssreki1@naver.com>
2022-10-28 13:44:07 +0900

    [REST API] MavenSpringExercise
    dependency 추가 및 yml 파일 수정
    UserDao 만들기
    User 도메인 만들

--- src/main/java/com/example/hello/dao/UserDao.java
+++ src/main/java/com/example/hello/dao/UserDao.java
@@ -12,11 +12,14 @@ import java.util.List;
 
 @Component
 public class UserDao {
+    private final DataSource dataSource;
     private final JdbcTemplate jdbcTemplate;
 
-    public UserDao(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    public UserDao(DataSource dataSource, JdbcTemplate jdbcTemplate) {
+        this.dataSource = dataSource;
+        this.jdbcTemplate = jdbcTemplate;
     }
+
     RowMapper<User> rowMapper = new RowMapper<User>() {
         @Override
         public User mapRow(ResultSet rs, int rowNum) throws SQLException {
