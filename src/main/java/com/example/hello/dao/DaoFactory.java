83be650432db18aea6687e9c767a1e9707884e01
dmdwns2 <ssreki1@naver.com>
2022-10-28 14:22:50 +0900

    [REST API] Maven SpringBoot로 CURD 게시판 API구현하기
    [H2] DaoFactory, dependency , pom.xml에 h2관련 추가 (1.4.200으로 변경)
     query.sql추가

--- src/main/java/com/example/hello/dao/DaoFactory.java
+++ src/main/java/com/example/hello/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package com.example.hello.dao;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
-
+@Configuration
 public class DaoFactory {
 
     //H2
