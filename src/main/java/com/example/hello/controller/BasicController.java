21b95c43ff62ba590c95b7233fa129bbfeee14f6
dmdwns2 <ssreki1@naver.com>
2022-10-28 11:41:24 +0900

    [RESTful API] MavenSpringExercise
    application.yml에 logging level 설정 코드 추가
    
    @Slf4j 어노테이션으로 호출시 로그 남김
    log.info

--- src/main/java/com/example/hello/controller/BasicController.java
+++ src/main/java/com/example/hello/controller/BasicController.java
@@ -1,20 +1,24 @@
 package com.example.hello.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class BasicController {
 
     @GetMapping("/api/hello1")
     public String hello1() {
-        return "hello";
+
+        log.info("hello1 요청이 들어왔습니다.");return "hello";
     }
 
     @GetMapping("/api/hello2")
     public String hello2(@RequestParam String param) {
-        return param;
+
+        log.info("hello2 요청이 들어왔습니다.");return param;
     }
 }
